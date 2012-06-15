fd6e4f1b7664981159ead1909dee16644a92850e
Andrew Kassen <atkassen@ucdavis.edu>
2012-06-15 09:09:07 -0700

    add rna support to translation

--- src/translate.py
+++ src/translate.py
@@ -1,20 +1,36 @@
-_gencode = {
+_gencode = 
 	'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+	'AUA':'I', 'AUC':'I', 'AUU':'I', 'AUG':'M',
 	'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+	'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T',
 	'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+	'AAC':'N', 'AAU':'N', 'AAA':'K', 'AAG':'K',
 	'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+	'AGC':'S', 'AGU':'S', 'AGA':'R', 'AGG':'R',
 	'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+	'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L',
 	'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+	'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P',
 	'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+	'CAC':'H', 'CAU':'H', 'CAA':'Q', 'CAG':'Q',
 	'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+	'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R',
 	'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+	'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V',
 	'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+	'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A',
 	'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+	'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E',
 	'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+	'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G',
 	'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+	'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S',
 	'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+	'UUC':'F', 'UUU':'F', 'UUA':'L', 'UUG':'L',
 	'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
-	'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
+	'UAC':'Y', 'UAU':'Y', 'UAA':'*', 'UAG':'*',
+	'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+	'UGC':'C', 'UGU':'C', 'UGA':'*', 'UGG':'W'}
 
 def translate(x):
   '''translate( sequence )
