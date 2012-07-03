cb291f4b1887ca91ca4b75ef1dfb685c0d494968
Andrew Kassen <atkassen@gmail.com>
2012-07-03 09:53:07 -0700

    changes to grepseq, setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 from distutils.core import setup
 
 setup(name="biotools",
-      version='1.2.1',
+      version='1.2.2',
       description="A bunch of bioinformatics utilities.",
       long_description="""Accompanies Bart, Rebecca, *et al.* High-throughput genomic sequencing of Cassava Bacterial Blight strains identifies conserved effectors to target for durable resistance. *PNAS Plus*.
 
-Currently depends on `clustalw <ftp://ftp.ebi.ac.uk/pub/software/clustalw2/2.1/>`_ and `BLAST <ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/>`_.""",
+Currently depends on `clustalw <ftp://ftp.ebi.ac.uk/pub/software/clustalw2/2.1/>`_ and `BLAST <ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/>`_.
+
+You can grab the most current code from `github <https://github.com/sonwell/biotools>`_ or via git
+    git clone git://github.com/sonwell/biotools.git""",
       author="Andrew Kassen",
       maintainer="Andrew Kassen",
       author_email="atkassen@gmail.com",
